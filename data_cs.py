71f36bebcabaad6ed917e71922a6fa139150ff76
dhruvmanjhi02-maker <dhruvmanjhi02@gmail.com>
2025-10-31 17:20:20 +0530

    data_cs.py

--- data_cs.py
+++ data_cs.py
@@ -1,9 +1,5 @@
 import json,os
 data = []
 for i in [x for x in os.listdir('bombers') if x[-4:]=="json"]:
-    with open(i, "r") as file:
+    with open(f"bombers/{i}", "r") as file:
         data.append(json.load(file))
-
-
-
-
